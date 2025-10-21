af013f36db83dc42875db57cc4f4cf31608a09b0
LUCIezq <lucieze02@icloud.com>
2025-10-21 12:25:27 -0300

    add Api nominatim+openstreetmap

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,7 @@
-const url = "https://nominatim.openstreetmap.org/reverse?";
-
 var map = L.map('map').setView([-34.579, -58.381], 7);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-map.on('click', function (e) {
+map.on('click', async function (e) {
     var lat = e.latlng.lat;
     var lon = e.latlng.lng;
 
@@ -12,18 +10,34 @@ map.on('click', function (e) {
     } else {
         marker = L.marker(e.latlng).addTo(map);
     }
+    console.log(await getLocation(lat, lon));
 });
 
 const getLocation = async (lat, lon) => {
+
+    const baseUrl = "http://localhost/services/proxy_nominatim.php";
+
+    if (typeof lat !== 'number' || typeof lon !== 'number') {
+        throw new Error("Las latitudes y longitudes deben ser números");
+    }
+
+    const params = new URLSearchParams({
+        lat: lat,
+        lon: lon
+    });
+
     try {
-        const response = await fetch(url + `lat=${lat}&lon=${lon}&format=json`);
+        const response = await fetch(`${baseUrl}?${params.toString()}`);
 
         if (!response.ok) {
             throw new Error("HTTP error " + response.status);
         }
+
         const data = await response.json();
+        console.log(data);
         return data;
+
     } catch (error) {
-        console.error("Error fetching location:", error);
+        console.error("Error al obtener los datos de la API:", error);
     }
 }
