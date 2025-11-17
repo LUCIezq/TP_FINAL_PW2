6a656b15c0e916f2edbdcaa9b4563ee61e25b6cf
Leandro <leandrocarrazcopedraza@gmail.com>
2025-11-16 23:20:23 -0300

    fixs

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -32,18 +32,18 @@ class AdminController
         $reporte = new ReporteAdmin($this->conexion);
 
         //Obtener datos para el dashboard
-        $data = [
-            "totalUsuarios"           => $reporte->getTotalUsuarios(),
-            "totalPartidas"           => $reporte->getTotalPartidas(),
-            "totalPreguntas"          => $reporte->getTotalPreguntas(),
-            "totalPreguntasUsuarios"  => $reporte->getTotalPreguntasUsuarios(),
+        // $data = [
+        //     "totalUsuarios"           => $reporte->getTotalUsuarios(),
+        //     "totalPartidas"           => $reporte->getTotalPartidas(),
+        //     "totalPreguntas"          => $reporte->getTotalPreguntas(),
+        //     "totalPreguntasUsuarios"  => $reporte->getTotalPreguntasUsuarios(),
 
-            // Gráficos
-            "usuariosPorPais"         => $reporte->getUsuariosPorPais(),
-            "usuariosPorSexo"         => $reporte->getUsuariosPorSexo(),
-            "usuariosPorEdad"         => $reporte->getUsuariosPorEdad(),
-            "porcentajeCorrectas"     => $reporte->getPorcentajeCorrectasPorUsuario()
-        ];
+        //     // Gráficos
+        //     "usuariosPorPais"         => $reporte->getUsuariosPorPais(),
+        //     "usuariosPorSexo"         => $reporte->getUsuariosPorSexo(),
+        //     "usuariosPorEdad"         => $reporte->getUsuariosPorEdad(),
+        //     "porcentajeCorrectas"     => $reporte->getPorcentajeCorrectasPorUsuario()
+        // ];
 
         // Renderizar la vista del dashboard
         $this->mustacheRenderer->render("adminVista", $data);
